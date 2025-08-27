1af04759be7a4f82ec380128fc6b0709f7dcdfdb
Cytanna <catwicks0@gmail.com>
2025-08-26 21:19:53 -0400

    redo

--- html_1/index.js
+++ html_1/index.js
@@ -1,22 +1,31 @@
-if (window.location.pathname.includes("sign.up.html")) {
+if (window.location.pathname.includes("sign_up.html")) {
   const form = document.getElementById("usercreation");
 
-  if (form) {
+  if (form){
+
     form.addEventListener("submit", function (e) {
       e.preventDefault();
-
+      
       const username = document.getElementById("usercreate").value;
       const email = document.getElementById("enteremail").value;
       const password = document.getElementById("passwordcreate").value;
-
+      
       console.log("Username:", username);
       console.log("Email:", email);
       console.log("Password:", password);
-
+      
       //document.getElementById("message").textContent = `Account created for ${username}!`;
+      localStorage.setItem("username", username);
       window.location.href = "welcome.html";
-
     });
   }
 }
-
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.location.pathname.includes("welcome.html")) {
+    const name = localStorage.getItem("username");
+    const welcome = document.getElementById("welcomeMessage");
+    if (name && welcome) {
+      welcome.textContent = `Welcome, ${name}! to Cats Project!`;
+    }
+  }      
+});
